0e278d614c729c0c42e1675951d17b7d6ae67e4f
Beatriz <40210313@esmad.ipp.pt>
2025-03-12 11:43:34 +0000

    Initialize project with Express, MongoDB, and dotenv configuration

---
+++ config/db.config.js
@@ -0,0 +1,10 @@
+const config = {
+  USER: process.env.DB_USER,
+  PASSWORD: process.env.DB_PASSWORD,
+  DB: process.env.DB_NAME,
+  HOST: process.env.DB_HOST,
+  SECRET: process.env.SECRET,
+};
+config.URL = `mongodb+srv://${config.USER}:${config.PASSWORD}@${config.HOST}/${config.DB}?retryWrites=true&w=majority`;
+
+module.exports = config;
