46a6831f211e5d99f329efc4c05d8dbb87f55080
MarianaV22 <marianaveiga@ipvc.pt>
2025-03-19 12:27:36 +0000

    bd local

--- config/db.config.js
+++ config/db.config.js
@@ -8,3 +8,23 @@ const config = {
 config.URL = `mongodb+srv://${config.USER}:${config.PASSWORD}@${config.HOST}/${config.DB}?retryWrites=true&w=majority`;
 
 module.exports = config;
+
+
+/*
+//ligacao para a local 
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, { //configurar a ligação à bd
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB conectado com sucesso!");
+  } catch (error) {
+    console.error("Erro ao conectar no MongoDB:", error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;*/
