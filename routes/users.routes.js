1eeacd4760b8e379c299dfbd2e54c6986817028b
Beatriz <40210313@esmad.ipp.pt>
2025-06-22 14:36:42 +0100

    feat: integrate Google authentication with Passport and update user routes
    - Updated package.json to include express-session, google-auth-library, passport, and passport-google-oauth20.
    - Added Google login route and controller to handle Google authentication.
    - Implemented Passport.js strategy for Google OAuth.
    - Refactored user routes to include Google login and maintain existing functionality.
    - Enhanced user creation logic to handle Google account details.

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -2,32 +2,27 @@ const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middlewares/auth.middleware");
 const usersController = require("../controllers/users.controller");
-//const authController = require("../controllers/auth.controller");
-
-/*
-router.use((req, res, next) => {
-  const start = Date.now();
-  res.on("finish", () => {
-    // finish event is emitted once the response is sent to the client
-    const diffSeconds = (Date.now() - start) / 1000; // figure out how many seconds elapsed
-    console.log(
-      `${req.method} ${req.originalUrl} completed in ${diffSeconds} seconds`
-    );
-  });
-  next();
-});    */
+const googleController = require("../controllers/google.controller");
 
 router.route("/signup").post(usersController.createUser);
 
-router.route("/")
-  .get(authMiddleware, (req, res, next) => {
-   
-    if (req.user.role !== 'admin') {
-      return res.status(403).json({ message: 'Acesso negado. Somente administradores podem acessar essa rota.' });
-    }
-    next(); 
-  }, usersController.findAll);
+router.route("/google-login").post(googleController.login);
 
+router.route("/").get(
+  authMiddleware,
+  (req, res, next) => {
+    if (req.user.role !== "admin") {
+      return res
+        .status(403)
+        .json({
+          message:
+            "Acesso negado. Somente administradores podem acessar essa rota.",
+        });
+    }
+    next();
+  },
+  usersController.findAll
+);
 
 router.post("/login", usersController.login);
 
@@ -38,18 +33,25 @@ router.put("/:id", authMiddleware, usersController.updateUser);
 router.get("/partner", authMiddleware, usersController.getPartner);
 router.get("/me", authMiddleware, usersController.getLoggedInUser);
 
-router.get('/accessories', authMiddleware, usersController.getUserAccessories);
+router.get("/accessories", authMiddleware, usersController.getUserAccessories);
 
-router.post('/accessories', authMiddleware, usersController.buyAccessory);
+router.post("/accessories", authMiddleware, usersController.buyAccessory);
 
-router.put('/accessories/equip',authMiddleware,usersController.equipAccessory);
+router.put(
+  "/accessories/equip",
+  authMiddleware,
+  usersController.equipAccessory
+);
 
-router.get( "/accessories-equipped",authMiddleware,usersController.getEquippedAccessories);
+router.get(
+  "/accessories-equipped",
+  authMiddleware,
+  usersController.getEquippedAccessories
+);
 
+router.delete("/:id", authMiddleware, usersController.deleteUser);
 
-router.delete('/:id', authMiddleware, usersController.deleteUser);
-
-router.get('/stats/users', authMiddleware, usersController.getUserStats);
+router.get("/stats/users", authMiddleware, usersController.getUserStats);
 
 // router
 //   .route("/users/:userID")
